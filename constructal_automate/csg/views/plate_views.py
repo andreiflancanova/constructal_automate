7b46894c4c2d3c965d4b9f687f2b31f37767141e
Andrei Ferreira Lançanova <andreiflancanova@hotmail.com>
2024-06-04 00:11:46 -0300

    STYLE: Adicionando spaces para Flake8

--- constructal_automate/csg/views/plate_views.py
+++ constructal_automate/csg/views/plate_views.py
@@ -4,25 +4,26 @@ from django.shortcuts import get_object_or_404
 from csg.models import Plate
 from csg.serializers import PlateSerializer
 
+
 class PlateViewSet(viewsets.ModelViewSet):
     def list(self, request):
         queryset = Plate.objects.all()
         serializer = PlateSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def retrieve(self, request, pk=None):
         queryset = Plate.objects.all()
         plate = get_object_or_404(queryset, pk=pk)
         serializer = PlateSerializer(plate)
         return Response(serializer.data)
-    
+
     def create(self, request):
         serializer = PlateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-    
+
     def update(self, request, pk=None):
         queryset = Plate.objects.all()
         plate = get_object_or_404(queryset, pk=pk)
@@ -31,7 +32,7 @@ class PlateViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
-    
+
     def destroy(self, request, pk=None):
         queryset = Plate.objects.all()
         my_model = get_object_or_404(queryset, pk=pk)
