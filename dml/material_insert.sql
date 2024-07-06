8bf02f7e5f9e79b061525d51c562fa8cfd432355
Andrei Ferreira Lançanova <andreiflancanova@hotmail.com>
2024-07-06 17:40:27 -0300

    FEAT: CBEB initial DB migrations

---
+++ dml/material_insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO `constructal_automate_results`.`cbeb_material`
+(`id`,
+`name`,
+`young_modulus`,
+`poisson_ration`,
+`yielding_stress`)
+VALUES
+(1,
+'Aço AH-36',
+210000,
+0.3,
+355);
