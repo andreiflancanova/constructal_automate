7c9b3ca9b108d417dd60f230a3900e5f6dd1408f
Andrei Ferreira Lançanova <andreiflancanova@hotmail.com>
2024-10-13 01:40:39 -0300

    FEAT: Enriching entities to handle equivalent length and area of stiffened plates

--- constructal_automate/cbeb/models/elastic_buckling.py
+++ constructal_automate/cbeb/models/elastic_buckling.py
@@ -5,8 +5,7 @@ from .stiffened_plate_analysis import StiffenedPlateAnalysis
 class ElasticBuckling(models.Model):
     id = models.BigAutoField(primary_key=True)
     stiffened_plate_analysis = models.ForeignKey(StiffenedPlateAnalysis, on_delete=models.CASCADE)
-    n_x = models.DecimalField(max_digits=7, decimal_places=3) 
-    csi_y = models.DecimalField(max_digits=4, decimal_places=3) 
     n_cr = models.DecimalField(max_digits=7, decimal_places=2, null=True)
-    sigma_cr = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    sigma_cr_ts = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    sigma_cr_ls = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     w_center = models.DecimalField(max_digits=8, decimal_places=4, null=True)
--- constructal_automate/cbeb/models/elasto_plastic_buckling.py
+++ constructal_automate/cbeb/models/elasto_plastic_buckling.py
@@ -5,8 +5,11 @@ from .stiffened_plate_analysis import StiffenedPlateAnalysis
 class ElastoPlasticBuckling(models.Model):
     id = models.BigAutoField(primary_key=True)
     stiffened_plate_analysis = models.ForeignKey(StiffenedPlateAnalysis, on_delete=models.CASCADE)
-    n_u = models.DecimalField(max_digits=7, decimal_places=2, null=True)
-    sigma_u = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    p_u_ts = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    p_u_ls = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    n_u = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    sigma_u_ts = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    sigma_u_ls = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     w_max = models.DecimalField(max_digits=8, decimal_places=4, null=True)
     w_dist_img_path = models.TextField(null=True)
     von_mises_dist_img_path = models.TextField(null=True)
