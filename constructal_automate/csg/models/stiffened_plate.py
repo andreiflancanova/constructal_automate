7bdf1315d1bc2d89547e8304d0b2eae7a48a9656
Andrei Ferreira Lançanova <andreiflancanova@hotmail.com>
2024-10-26 13:48:38 -0300

    FEAT: Including BucklingLoadType and Mesh Size in analysis filename

--- constructal_automate/csg/models/stiffened_plate.py
+++ constructal_automate/csg/models/stiffened_plate.py
@@ -5,7 +5,7 @@ from .plate import Plate
 class StiffenedPlate(models.Model):
     id = models.BigAutoField(primary_key=True)
     plate = models.ForeignKey(Plate, on_delete=models.CASCADE)
-    phi = models.DecimalField(max_digits=5, decimal_places=4)
+    phi = models.DecimalField(max_digits=3, decimal_places=2)
     N_ls = models.IntegerField()
     N_ts = models.IntegerField()
     k = models.DecimalField(max_digits=6, decimal_places=3)
